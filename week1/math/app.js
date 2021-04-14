ec4112aa04c3014c5a8eb8a4bf6a57df3b3e71c0
Mark Olivo <markolivo@Marks-iMac.local>
2021-04-14 12:44:15 -0400

    Node Modules  basics & Patterns

---
+++ week1/math/app.js
@@ -0,0 +1,7 @@
+const number = require('./math');
+
+console.log(number);
+console.log(number.plus(7, 11));
+console.log(number.minus(7, 11));
+console.log(number.multiply(7, 11));
+console.log(number.div(7, 11));
---
+++ week1/math/math.js
@@ -0,0 +1,23 @@
+
+
+ const plus = (a, b) => {
+     return a + b
+ }
+
+ const minus = (c, d) => {
+     return c - d
+ };
+ 
+const multiply = (e, f) => {
+    return e * f
+};
+
+
+const div = (g, h) => {
+     return g / h
+ };
+ 
+
+ module.exports ={
+    plus, minus, multiply, div
+ };
