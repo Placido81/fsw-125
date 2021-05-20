3a7fe4f4eeb1cf93d5a24f01263249ab67ffbece
Mark Olivo <markolivo@Marks-iMac.local>
2021-05-20 15:40:00 -0400

    final copy

--- week6/thing-finder/server.js
+++ week6/thing-finder/server.js
@@ -2,10 +2,9 @@ const express = require('express'); // Importing Express
 const app = express(); // Declaring Server Variable
 const morgan = require('morgan'); // Importing Morganrs
 // ------------------------------------------- Installing Express-Query-Boolean
-const booleanParser = require('express-query-boolean');
+const PORT = 7000
 // Middleware (for every request) // Looks For A Request Body, And Turns It Into 'req.body'
     app.use(express.json());
-    app.use(booleanParser());
     app.use(morgan('dev')); // Logs Request To The Console
 // ---------------------------------------------------------------- Setting More Middle Ware
 // ---------------------------------------------------------------- Server Imports
@@ -19,6 +18,6 @@ app.use((err, req, res, next) => {
 
 // ------------------------------------------------------------------- Server Is Set To Listen
 //  -------------------Two Arguments: PORT, CB
-app.listen(7000, () => {
-    console.log('The Server Is Live!!!!'); 
-});
+app.listen(PORT, () => {
+    console.log(`Server running on Port: ${PORT}`);
+})
